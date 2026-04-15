1d901178e2dfd8e72c500f713be301ed3df10474
Shamsher Ali <130893624+shamsherali-dev@users.noreply.github.com>
2026-04-15 00:22:55 -0700

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,23 @@
+# print("hello world");
+# print("my name is  Shamsher \n my \t edu BSCS ");
+
+# print("Shamsher's channel ");
+
+
+name = "Shamsher";
+age = 26;
+edu='BSCS'; 
+# initilize 
+
+# print name and age 
+
+"""
+shamsher 
+     26 
+      sdsfdf
+"""
+  
+print(name,age);
+
+# print your name , age , and education 
+
