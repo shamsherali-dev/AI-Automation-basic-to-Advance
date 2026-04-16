9a869c9bf2adb0d3ed6c01eab40b45c313a74ad6
Shamsher Ali <130893624+shamsherali-dev@users.noreply.github.com>
2026-04-15 23:25:21 -0700

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,165 @@
+
+# name = "Shamsher";
+# age = 26;
+# print("type is = ",type(name));
+# print("age Type is = ",type(age))
+
+# val = 3.445;
+# print("val type is = ",type(val));
+
+# vl = True;
+# vl2 = False;
+# vl3=None;
+# print("vl type is = ",type(vl));
+# print("vl2 type is = ",type(vl2));
+# print("vl3 type is = ",type(vl3));
+# """
+#  print vl1
+#  print vl2
+#  print vl3
+# """
+
+
+#  lecture 2 ::
+# print(type(name));
+# print(type(age));
+
+# price=2.333;
+# print(type(price)); 
+# puthon is case sensitive lanaguage , : like Apple , apple , these both are different variable ;
+
+# a= 2 
+# b=3
+# sum=a+b;
+# dif=a-b;
+# print(sum);
+# print(dif);
+
+#   2 + 3 =  2,3 are oprands , + is operator ;
+
+a=6
+b=2
+# sum = a+b;
+# print("the sum is = ",sum);
+# div = a/b;
+# print("the divd is = ",div);
+# print("the type is = ",type(div))
+# print("the type is = ",type(DIV))
+
+# mod=a%b;
+# print("module is = ",mod);
+
+# WAP that perform all arthematic operation on 2 value; 
+# arithmetic operators ;
+# c = 4;
+# d = 7;
+# print(c+d)
+# print(c-d)
+# print(c*d)
+# print(c/d)
+# print(c%d); 
+# % for remanders find called modules;
+# print(c**d); 
+# ** is called power oerator , c^d , like 2^3 = 8
+
+#relational operator / comparison operator ::
+# f = 50
+# g=23
+# print (f==g)
+# print (f!=g)
+# print(f>g)
+# print(f<g)
+# print(f>=g)
+# print(f<=g)
+
+# Assignment Operator :: 
+# (=,=+,-=,/=,%=,**=)
+
+num =2; 
+# called initilize 
+# num=num+2;
+# num+=2;
+# num*=2;
+# num/=2;
+# num%=2;
+# print("num",num);
+
+#Logic Operator :
+#  (not , and , or);
+
+# a =20;
+# b=10;
+# print(not a>b);
+# print( a<b);
+# print( not a<b);
+
+# val1=True
+# val2=True
+
+# print('AND , result is = ', val1 and val2);
+
+# val1=False
+# val2=True
+# print('OR , result is = ', val1 or val2);
+
+# type conversion 
+# 2 types of conversion :
+#  conversion :: automatically , Implicit = automatic / understood without saying
+# casting :: manually , zabrdasti , Explicit = manual / stated clearly
+a=2;
+b= 4.325;
+sum=a+b;
+print( sum);
+
+# casting
+# a=2;
+# b= 4.325;
+# sum=int(a+b);
+# print( sum);
+
+# a= int("3")
+# b=2;
+# sum=b +b;
+# print("type is = ",type(sum),", sum is = ", sum);
+# sum=str(sum);
+# print("type is = ",type(sum),", sum is = ", sum);
+
+
+#  user input :: 
+
+# note input value , always in str ;
+# name = input("please enter name : ");
+# age=int(input("please enter age = "));
+# print(type(name),type(age));
+
+
+# practice ::
+#  input 2 number and print therir sum ::
+# v1=int(input("enter the value 1 = "))
+# v2=int(input("enter the value 2 = "))
+# sum = v1+v2;
+# print("the sum is = ",sum);
+
+
+# input side of square and print area ; 
+
+# val=float(input("enter the value 1 = "))
+# area=val*val;
+# # area=val**2;
+# print("the area is = ", area);
+
+# input 2 floating point numbers and print their average .
+# v1=float(input("enter the value = "))
+# v2=float(input("enter the value = "))
+# avg= (v1+v2)/2;
+# print("the avg is = ", avg);
+
+#WAP to input 2 int numbers a, b; print True if a is greator than or equal to b , if not print false ;
+# v1=int(input("enter the value = "))
+# v2=int(input("enter the value = "))
+# print("RESULT IS = ",v1>=v2);
+
+
+
+
+
