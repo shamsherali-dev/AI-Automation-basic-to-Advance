2ba65d0c224fa8f7f5388cf201106e57a5a12c83
Shamsher Ali <130893624+shamsherali-dev@users.noreply.github.com>
2026-04-17 10:28:58 -0700

    Add files via upload

---
+++ practice3.py
@@ -0,0 +1,65 @@
+
+
+# ** :: power operator , 4^3 = 4*4*4
+# print("the power is = ",v1**v2);
+
+#  relation operator ; 
+# == , != , > , < , >= , <=
+
+# print(v1==v2);
+# print(v1!=v2);
+# print(v1>v2);
+# print(v1<v2);
+# print(v1>=v2);
+
+# Assignment Operator :: 
+# (=,+=,-=,/=,%=,**=)
+
+v1=6;
+# v1+=2;
+# v1=v1+2;  
+# v1-=1;  
+# v1/=3;  
+# print("the v1 = ",v1);
+# print("the type = ",type(v1));
+
+#Logic Operator :
+#  (not , and , or);
+
+# a=False;
+# b=True;
+
+# print(a and b);
+# print(a or b);
+# print( not (a or b));
+
+
+# type conversion 
+# 2 types of conversion :
+#  conversion :: automatically , Implicit = automatic / understood without saying
+# casting :: manually , zabrdasti , Explicit = manual / stated clearly
+# a=2;
+# b= 4.325;
+# print(a+b)
+# print(type(a+b))
+# sum = int(a+b);
+# sum = str(a+b);
+
+# print("the sum is = ",sum)
+# print("the sum type is = ",type(sum))
+
+# name = input("enter the name = ");
+# print("the name is = ", name);
+# print("the name is = ", type(name));
+
+# age = int (input("enter age = "))
+# print("the age is ",age);
+# print("the age type is ",type(age));
+
+
+# assignment 
+ # practice ::
+#  input 2 number and print therir sum ; 
+#  input side of square and print area ;  
+# input 2 floating point numbers and print their average .
+#WAP to input 2 int numbers a, b; print True if a is greator than or equal to b (>=), if not print false ;
