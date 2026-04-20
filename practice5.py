8d5d3479e55eb126f7575bfd052862a9ef890b6e
Shamsher Ali <130893624+shamsherali-dev@users.noreply.github.com>
2026-04-20 08:15:40 -0700

    AI automation python 5

---
+++ practice5.py
@@ -0,0 +1,61 @@
+
+# string  functions ::
+ 
+v1 = "i am Python Developer Python"
+# endswith()
+# print(v1.endswith("on"))
+
+# capitalize();
+# print(v1.capitalize());
+
+# replace("older_text","newText");
+# print(v1.replace("Python","C++"))
+
+
+# find("");
+# print(v1.find("a"));
+
+
+# count()
+about = " am Shamsher ali";
+# print(about.count("am"));
+
+
+# Assignment :: 
+# WAP to input name and print its length 
+#WAP to find occurance of "s' in string and los how many time ocur
+
+ 
+#  if else
+# nb=-2
+# if(nb>0):
+#     print("greator");
+# else:
+#     print("less"); 
+
+# #  if  elif else 
+
+# nb =int(input("enter the number "))
+
+# if(nb>2):
+#     print("greator than 2");
+# elif(nb<2):
+#     print("less than 2");
+# elif(nb==6):
+#     print("nb = 6");
+# else:
+#     print("wrong input ");
+
+marks=int(input("enter the marks "));
+
+if(marks>90):
+    print("the grade is = A");
+elif(marks>80):
+ print("the grade is = B");
+elif(marks>70):
+   print("the grade is = C"); 
+else:
+   print("student fail");   
+
+#  num%2==0  , even  , odd
+
